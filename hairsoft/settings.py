2ea127423b3dfd13e72f8af0594ba480aa928d05
Luciano Bauer <lucianobauer13@gmail.com>
2026-01-09 23:56:41 -0300

    Eliminar SessionAuth para fix mobile

--- hairsoft/settings.py
+++ hairsoft/settings.py
@@ -261,7 +261,7 @@ MERCADO_PAGO = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny', # Ojo: Permite acceso público por defecto, ajustalo en las vistas
